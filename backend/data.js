c62abdf39b310f53d09fa30f7e01974ddd5ed248
Lavit Samson <lavitsamson@hotmail.com>
2020-11-29 23:24:54 +0530

    share code on github

---
+++ backend/data.js
@@ -0,0 +1,83 @@
+const data = {
+  products: [
+    {
+      _id: "1",
+      name: "Nike Slim Shirt",
+      category: "shirts",
+      image: "/images/p1.jpg",
+      price: "120",
+      countInStock: "10",
+      brand: "Nike",
+      rating: "4.5",
+      numReviews: "10",
+      description: "high quality product",
+    },
+
+    {
+      _id: "2",
+      name: "Allen Solly Shirt",
+      category: "shirts",
+      image: "/images/p1.jpg",
+      price: "120",
+      countInStock: "20",
+      brand: "Allen Solly",
+      rating: "4.5",
+      numReviews: "10",
+      description: "high quality product",
+    },
+
+    {
+      _id: "3",
+      name: "Peter England Shirt",
+      category: "shirts",
+      image: "/images/p1.jpg",
+      price: "120",
+      countInStock: "0",
+      brand: "Peter England",
+      rating: "1",
+      numReviews: "10",
+      description: "high quality product",
+    },
+
+    {
+      _id: "4",
+      name: "Adidas Regular Shirt",
+      category: "shirts",
+      image: "/images/p1.jpg",
+      price: "120",
+      countInStock: "15",
+      brand: "Adidas",
+      rating: "3.5",
+      numReviews: "10",
+      description: "high quality product",
+    },
+
+    {
+      _id: "5",
+      name: "Lacoste T-Shirt",
+      category: "T-Shirts",
+      image: "/images/p1.jpg",
+      price: "80",
+      countInStock: "0",
+      brand: "Lacoste",
+      rating: "4",
+      numReviews: "10",
+      description: "high quality product",
+    },
+
+    {
+      _id: "6",
+      name: "Polo Fine Shirt",
+      category: "shirts",
+      image: "/images/p1.jpg",
+      price: "220",
+      countInStock: "30",
+      brand: "Polo",
+      rating: "2.5",
+      numReviews: "10",
+      description: "high quality product",
+    },
+  ],
+};
+
+export default data;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,44 +1,29 @@
-import data from './data';
+import { BrowserRouter, Route } from 'react-router-dom';
+import HomeScreen from './screen/HomeScreen';
+import ProductScreen from './screen/ProductScreen';
 
 function App() {
   return (
-    <div className="grid-container">
-        <header className="row">
-            <div>
-                <a className="brand" href="/">amazona</a>
-            </div>
-            <div>
-                <a href="/cart">Cart</a>
-                <a href="/signin">Sign In</a>
-            </div>
+    <BrowserRouter>
+      <div className='grid-container'>
+        <header className='row'>
+          <div>
+            <a className='brand' href='/'>
+              amazona
+            </a>
+          </div>
+          <div>
+            <a href='/cart'>Cart</a>
+            <a href='/signin'>Sign In</a>
+          </div>
         </header>
         <main>
-            <div className="row center">
-                {
-                    data.products.map((product) =>(
-                        <div key={ product._id } className="card">
-                    <a href={ `/product/${product._id}` }>
-                        <img className="medium" src={ product.image } alt="Product" />
-                    </a>
-                    <div className="card-body">
-                    <a href={ `/product/${product._id}` }><h2>{ product.name }</h2></a>
-                        <div className="ratings">
-                            <span><i className="fa fa-star"></i></span>
-                            <span><i className="fa fa-star"></i></span>
-                            <span><i className="fa fa-star"></i></span>
-                            <span><i className="fa fa-star"></i></span>
-                            <span><i className="fa fa-star"></i></span>
-                        </div>
-                    <div className="price">${ product.price }</div>
-                    </div>
-                </div>
-                    ))
-                }
-                
-            </div>
+          <Route path='/product/:id' component={ProductScreen}></Route>
+          <Route path='/' component={HomeScreen} exact></Route>
         </main>
-        <footer className="row center">All rights reserved.</footer>
-    </div>
+        <footer className='row center'>All rights reserved.</footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
